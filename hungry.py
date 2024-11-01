d785a183096f1a74256b3221aa36e177a6d1c7f2
RaviRocksAI <ravipoliceai@outlook.com>
2024-11-01 22:09:35 +0530

    Update hungry.py
    added changes

--- hungry.py
+++ hungry.py
@@ -1,5 +1,6 @@
 hungry=input("are you hungry?")
-if hungry=="yes" and hungry=="y" and hungry=="Y":
+if hungry=="yes" and hungry=="y" and hungry=="Y"  
     print("eat samosa")
+    print("changes")
 else:
     print("do homework")    
